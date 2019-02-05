fc852d2f0b1a291228f5fd0f27b26ea9f98567b9
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-05 21:10:50 +0800

    Update login.vue

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,66 +1,103 @@
 <template>
-    <div class="main">
-        <el-row class="title">跨境平台—厂商管理系统</el-row>
-        <el-row class="input_area">
-            <el-form>
-                <el-row>
-                    <el-col :span="4" :offset="10">
-                        <el-form-item label="邮箱" v-model="login_email">
-                            <el-input placeholder="请输入登录邮箱"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
+  <div class="main">
+    <el-row class="title">跨境平台—厂商管理系统</el-row>
+    <el-row class="input_area">
 
-                <el-row>
-                    <el-col :span="4" :offset="10">
-                        <el-form-item label="密码" v-model="login_pwd">
-                            <el-input type="password" placeholder="请输入登录密码"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
+      <el-form :rules="rules" :model="login" ref="login">
+        <el-row>
+          <el-col class="form_border" :span="8" :offset="8">
+            <el-row>
+              <el-col :span="20" :offset="2">
+                <el-form-item label="邮箱" prop="login_email">
+                  <el-input class="input-with-select" v-model="login.login_email" placeholder="请输入登录邮箱" @keydown.enter.native="submitForm('login')">
 
-                <el-row>
-                    <el-col :span="4" :offset="10">
-                        验证
-                        <el-slider v-model="check_num"></el-slider>
-                        <span style="color:#D7E1EB">请将滑块移动到 {{verify}}</span>
+                  </el-input>
+                </el-form-item>
 
-                    </el-col>
-                </el-row>
-                <el-row style="color:#D7E1EB">
-                    <el-col :span="4" :offset="10">
-                        目前 {{check_num}} {{verify_check}}
-                        <span v-if="verify_status" class="el-icon-circle-check-outline" style="padding-left:10px;color:#D7E1EB">匹配</span>
-                    </el-col>
-                </el-row>
-                <el-row style="padding-top:20px;text-align:right;font-size:12px;cursor: pointer">
-                    <el-col :span="4" :offset="10">
-                        忘记密码?
-                    </el-col>
-                </el-row>
-                <el-row style="padding-top:20px;text-align:right;font-size:12px;cursor: pointer">
-                    <el-col :span="4" :offset="10">
-                        还没有账号？赶紧申请一个吧
-                    </el-col>
-                </el-row>
+              </el-col>
+            </el-row>
 
-                <el-row style="padding-top:20px;">
-                    <el-col :span="3" :offset="11">
-                        <el-form-item>
-                            <el-button type="primary">登录</el-button>
-                            <el-button>清空</el-button>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-            </el-form>
+            <el-row>
+              <el-col :span="20" :offset="2">
+                <el-form-item label="密码" prop="login_pwd">
+                  <el-input v-model="login.login_pwd" type="password" placeholder="请输入登录密码" @keydown.enter.native="submitForm('login')">
+                  </el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row style="text-align:right;font-size:12px;">
+              <el-col :span="20" :offset="2" style="cursor: pointer">
+                忘记密码?
+              </el-col>
+            </el-row>
+            <el-row style="padding-top:10px;">
+              <el-col :span="20" :offset="2">
+                <el-form-item>
+                  <el-button style="width:100%" type="primary" @click="submitForm('login')">登录</el-button>
+                </el-form-item>
+              </el-col>
+            </el-row>
+
+            <el-row style="text-align:right;font-size:12px;">
+               <router-link to="/register" tag="span">
+              <el-col :span="20" :offset="2" style="cursor: pointer;padding-bottom:50px;">
+                还没有账号？赶紧申请一个吧
+              </el-col>
+               </router-link>
+            </el-row>
+          </el-col>
         </el-row>
-    </div>
+      </el-form>
+    </el-row>
+
+    <el-row class="footer">
+      <el-col :span="3" :offset="9">Copyright：
+        <span class="iconfont icon-banquan"></span>猿聚团队</el-col>
+      <el-col :span="3">version 0.1.0</el-col>
+    </el-row>
+
+    <el-dialog title="验证" top=35vh :show-close=show_close :close-on-click-modal=false :modal=false :visible.sync="verify_show" width="30%">
+      <span>
+        <el-row>
+          <el-col :span="24">
+
+            <el-slider v-model="check_num" :disabled="disabled"></el-slider>
+            <span>请将滑块移动到 100</span>
+
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="4">
+            目前 {{check_num}} {{verify_check}}
+            <br>
+            <!-- <span v-if="verify_status" class="el-icon-circle-check-outline" style="color:#D7E1EB">匹配</span> -->
+          </el-col>
+        </el-row>
+        <el-row style="text-align:right">
+          <el-button @click="close_dialog">关 闭</el-button>
+        </el-row>
+      </span>
+    </el-dialog>
+
+  </div>
+
 </template>
 <style scoped>
-body {
+body,
+html {
   padding: 0;
   margin: 0;
 }
+.footer {
+  width: 100%;
+  height: 80px;
+  margin-top: 80px;
+  border-top: 2px solid #e9e9e9;
+  background-color: #f8f8f8;
+  text-align: center;
+  line-height: 80px;
+}
+
 .el-form-item__label {
   color: white;
 }
@@ -71,39 +108,101 @@ body {
 }
 .input_area {
   padding-top: 50px;
+  height: 100%;
+}
+.form_border {
+  background-color: white;
+  -moz-box-shadow: 4px 4px 10px #333333;
+  -webkit-box-shadow: 4px 4px 10px #333333;
+  box-shadow: 4px 4px 10px #333333;
+  padding-top: 30px;
 }
 </style>
 <script>
 export default {
   data() {
+    // 邮箱格式验证
+    var checkEmail = (rule, value, callback) => {
+      const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+      if (!value) {
+        return callback(new Error("登录邮箱不得为空"));
+      }
+      setTimeout(() => {
+        if (mailReg.test(value)) {
+          callback();
+        } else {
+          callback(new Error("请输入正确的邮箱格式"));
+        }
+      }, 100);
+    };
+    // 数据返回
     return {
-      verify_status: false,
-      verify_num: null,
-      check_num: null,
-      login_email: "",
-      login_pwd: "",
-      note: {}
+      show_close: false, //不显示对话框的关闭按钮
+      verify_status: false, //展示验证成功
+      verify_show: false, //验证框的显示
+      check_num: null, //滑块的数值
+      disabled: false, //登录验证
+      login: {
+        login_email: "", //邮箱登录数据
+        login_pwd: "" //登录密码数据
+      },
+
+      rules: {
+        login_email: [
+          { validator: checkEmail, required: true, trigger: "blur" } //邮箱的验证规则
+        ],
+        login_pwd: [
+          { required: true, message: "登录密码不得为空", trigger: "blur" } //密码的验证规则
+        ]
+      }
     };
   },
   computed: {
-    verify() {
-      var i = Math.random();
-      this.verify_num = parseInt(i * 100) + 1;
-      return this.verify_num;
-    },
     verify_check() {
-      if (this.check_num == this.verify_num) {
+      /**登录滑块验证  */
+      if (this.check_num == 100) {
         this.verify_status = true;
+        this.disabled = true;
+
+        setTimeout(() => {
+          this.$message({
+            message: "验证成功,登陆ing",
+            type: "success"
+          });
+          this.verify_show = false;
+          this.check_num = 0;
+          setTimeout(() => {
+            this.$router.push({ path: "/" }); /**路由跳转到系统首页 */
+          }, 1200);
+        }, 1000);
+        return;
       } else {
         this.verify_status = false;
+        this.disabled = false;
+        return;
       }
     }
   },
-  methods: {},
+  methods: {
+    close_dialog() {
+      this.verify_show = false;
+      this.check_num = 0;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.verify_show = true;
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  },
   mounted() {
     document
       .querySelector("body")
-      .setAttribute("style", "background-color:#0BC9C9");
+      .setAttribute("style", "background-color:#efefef");
   },
   beforeDestroy() {
     document.querySelector("body").removeAttribute("style");
