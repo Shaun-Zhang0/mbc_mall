8e4786d5cc96f06694307cb86529a24f21186863
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-05 21:10:35 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -7,20 +7,61 @@ import Index from "./components/structure/home.vue";
 import Release_product from "./components/product/release_product.vue";
 import Productlist from "./components/product/productlist.vue";
 import Update_info from "./components/information/update_info.vue";
-import Chargerback from "./components/order/chargeback.vue"
-
+import Chargerback from "./components/order/chargeback.vue";
+import Login from "./components/login/login.vue";
+import register from "./components/register/register.vue";
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {path:"/",component:Index},
-    {path:"/update_info",name:Update_info,component:Update_info},
-    {path:"/orderlist",name:Orderlist,component:Orderlist},
-    {path:"/release_product",name:Release_product,component:Release_product},
-    {path:"/Productlist",name:Productlist,component:Productlist},
-    {path:"/Chargerback",name:Chargerback,component:Chargerback}
-   
+    {
+      path: "/",
+      component: Index,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/update_info",
+      name: Update_info,
+      component: Update_info,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/orderlist",
+      name: Orderlist,
+      component: Orderlist,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/release_product",
+      name: Release_product,
+      component: Release_product,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/Productlist",
+      name: Productlist,
+      component: Productlist,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/Chargerback",
+      name: Chargerback,
+      component: Chargerback,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/Login",
+      name: Login,
+      component: Login,
+      meta: { keepAlive: false }
+    },
+    {
+      path: "/register",
+      name: register,
+      component: register,
+      meta: { keepAlive: false }
+    }
   ]
 });
