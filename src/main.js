2b0e186c7e12492a5e76733528e30834c5cbb617
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-12 17:41:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,17 +4,18 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import "./plugins/element.js";
-import login from "./components/login/login.vue"; //导入登录的组件
-import register from "./components/register/register.vue"; //导入注册的组件
+
 import header from "./components/structure/header.vue"; //导入顶部导航栏的组件
 import aside from "./components/structure/aside.vue"; //导入侧边导航栏的组件
 import main from "./components/structure/home.vue"; //导入首页的页面的组件
 import footer from "./components/structure/footer.vue"; //导入首页的页面的组件
-import update_info from "./components/information/update_info.vue"; //导入更新厂商界面的组件
-import release_product from "./components/product/release_product.vue"; //导入发布商品界面的组件
-import productlist from "./components/product/productlist.vue"; //导入搜索商品的界面的组件
-import orderlist from "./components/order/orderlist.vue"; //导入编辑订单的组件
-import chargeback from "./components/order/chargeback.vue"; //导入退单的组件
+import update_info from "./components/brand/information/update_info.vue"; //导入更新厂商界面的组件
+import release_product from "./components/brand/product/release_product.vue"; //导入发布商品界面的组件
+import productlist from "./components/brand/product/productlist.vue"; //导入搜索商品的界面的组件
+import orderlist from "./components/brand/order/orderlist.vue"; //导入编辑订单的组件
+import chargeback from "./components/brand/order/chargeback.vue"; //导入退单的组件
+import login from "./components/brand/login/login.vue"; //导入登录的组件
+import register from "./components/brand/register/register.vue"; //导入注册的组件
 import fonts from "./assets/fonts/iconfont.css"; //导入字符型图案
 
 Vue.config.productionTip = false;
