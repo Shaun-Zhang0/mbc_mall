29636e557f6ea1a590f054fbe5213df687951160
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-14 22:10:33 +0800

    Create carousel.vue

---
+++ src/components/mall/index/carousel.vue
@@ -0,0 +1,96 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-02-14 14:31:00 
+ * @Last Modified by: Shaun.Zhang
+ * @Last Modified time: 2019-02-14 22:04:03
+ */
+<template>
+    <div>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="slide in slides">
+                    <img style="width: 100%;max-height:100%" :src="slide.img_url" />
+                </div>
+
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+</template>
+<style scoped>
+html,
+body {
+  position: relative;
+  height: 100%;
+}
+body {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
+}
+.swiper-container {
+  border-top: 5px solid #999;
+  width: 100%;
+  margin-top: 50px;
+}
+.swiper-slide {
+  background: gold;
+  height: 500px;
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+</style>
+<script>
+import Swiper from "swiper";
+import "swiper/dist/css/swiper.min.css";
+export default {
+  data() {
+    return {
+      slides: [
+        { id: 1, img_url: require("../../../../public/img/cover1.jpg") },
+        { id: 2, img_url: require("../../../../public/img/cover2.jpg") },
+        { id: 3, img_url: require("../../../../public/img/cover3.jpg") },
+        { id: 4, img_url: require("../../../../public/img/cover4.jpg") }
+      ]
+    };
+  },
+  mounted() {
+    var swiper = new Swiper(".swiper-container", {
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        hideOnClick: true,
+        dynamicBullets: false,
+        clickable: true,
+        renderBullet: function(index, className) {
+          return (
+            '<li style="height:5px; width:25px; border-radius: 0%;" class="' +
+            className +
+            '">' +
+            "</li>"
+          );
+        }
+      }
+    });
+  }
+};
+</script>
