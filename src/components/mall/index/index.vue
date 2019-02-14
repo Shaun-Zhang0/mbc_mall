d47cf3c0fda3df5c0af6b3edfb21e44d8a09133d
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-14 22:10:38 +0800

    Create index.vue

---
+++ src/components/mall/index/index.vue
@@ -0,0 +1,30 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-02-14 16:30:00 
+ * @Last Modified by: Shaun.Zhang
+ * @Last Modified time: 2019-02-14 21:58:13
+ */
+<template>
+    <div>
+        <indexHeader></indexHeader>
+         <indexSearch></indexSearch>
+        <indexCarousel></indexCarousel>
+       
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+import indexHeader from "./../index/header";
+import indexCarousel from "./../index/carousel";
+import indexSearch from "./../index/search";
+export default {
+  components: {
+    indexHeader: indexHeader,
+    indexCarousel: indexCarousel,
+    indexSearch: indexSearch
+
+  }
+};
+</script>
