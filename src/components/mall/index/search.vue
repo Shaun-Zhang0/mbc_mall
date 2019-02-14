0e47751bb8e9f781351176c881ab857cabc6a6dc
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-14 22:44:07 +0800

    Update search.vue

--- src/components/mall/index/search.vue
+++ src/components/mall/index/search.vue
@@ -2,24 +2,24 @@
  * @Author: Shaun.Zhang 
  * @Date: 2019-02-14 17:01:58 
  * @Last Modified by: Shaun.Zhang
- * @Last Modified time: 2019-02-14 22:08:46
+ * @Last Modified time: 2019-02-14 22:40:14
  */
 <template>
     <div>
         <el-row class="row">
             <el-col :xs="8" :sm="10" :md="10" :lg="1" :xl="1">&nbsp;</el-col>
             <el-col :xs="16" :sm="14" :md="14" :lg="4" style="height:100%">
-                <span style="font-size: 30px;">跨境平台</span>
+                <span style="font-size: 30px;color: rgb(253, 40, 40);">跨境平台</span>
             </el-col>
             <el-col :xs="2" :sm="7" :md="7" :lg="3" :xl="3">&nbsp;</el-col>
             <el-col :xs="20" :sm="10" :md="10" :lg="8" :xl="9" class="form">
                 <el-row>
-                    <el-input class="input">
-                        <el-button type="success" class="el-icon-search" slot="append">搜索</el-button>
+                    <el-input class="input" placeholder="搜索 商品/厂商">
+                        <el-button class="el-icon-search" style="background-color:rgb(253, 40, 40);color:white" slot="append">搜索</el-button>
                     </el-input>
                 </el-row>
                 <el-row style="line-height:30px;">
-                    <el-col :sm="4" :md="4" :lg="3" :xs="4" class="suggest" v-for="words in suggest_word">
+                    <el-col :xs="5" :sm="4" :md="4" :lg="3" :xl="3" class="suggest" v-for="words in suggest_word">
                         <span class="suggest_item" style="">{{words.word}}</span>
                     </el-col>
                 </el-row>
@@ -28,7 +28,7 @@
         </el-row>
     </div>
 </template>
-<style scoped>
+<style>
 .row {
   max-height: 150px;
   line-height: 100px;
@@ -49,11 +49,26 @@
   margin-bottom: 5px;
   font-size: 12px;
 }
+.el-input-group__append {
+  background-color: rgb(253, 40, 40) !important;
+  border: none !important;
+  border-radius: 0px;
+}
+.input {
+  border: 1px solid rgb(253, 40, 40);
+  border-radius: 4px;
+}
+.el-input__inner {
+  border: none !important;
+}
 .suggest_item {
   cursor: pointer;
 }
 .suggest_item:hover {
   color: #ccc;
+}
+.search_button {
+  background-color: rgb(253, 40, 40);
 }
 </style>
 <script>
