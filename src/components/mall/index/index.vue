6886d3927288ba0e75e19f5d36e41ff9484e1ab9
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-19 22:49:56 +0800

    Update index.vue

--- src/components/mall/index/index.vue
+++ src/components/mall/index/index.vue
@@ -5,13 +5,16 @@
  * @Last Modified time: 2019-02-16 16:32:16
  */
 <template>
+  <div>
+    <indexHeader></indexHeader>
+    <indexSearch></indexSearch>
     <div>
-        <indexHeader></indexHeader>
-         <indexSearch></indexSearch>
-         <div>
-        <indexCarousel></indexCarousel>
-       </div>
+      <indexCarousel></indexCarousel>
     </div>
+    <div>
+      <index-productline></index-productline>
+    </div>
+  </div>
 </template>
 <style scoped>
 
@@ -21,13 +24,14 @@ import indexHeader from "./../index/header";
 import indexCarousel from "./../index/carousel";
 import indexSearch from "./../index/search";
 import indexMenu from "./../index/menu";
+import indexProductline from "./../index/product_line";
 export default {
   components: {
     indexHeader: indexHeader,
     indexCarousel: indexCarousel,
     indexSearch: indexSearch,
     indexMenu: indexMenu,
-
+    indexProductline: indexProductline
   }
 };
 </script>
