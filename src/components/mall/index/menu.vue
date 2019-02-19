afb2e76a0bbeacb6f85629e2fca0f8754a5466e5
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-19 22:50:04 +0800

    Update menu.vue

--- src/components/mall/index/menu.vue
+++ src/components/mall/index/menu.vue
@@ -8,25 +8,25 @@
   <div style="width:100%">
     <el-row class="show" @mouseleave.native="expand = false">
       <el-col :xs="0" :sm="1" :md="1" :lg="3" :xl="4">&nbsp;</el-col>
-      <el-col class="menu_main"  :md="3" :lg="3" :span="3" :xs="7" >
+      <el-col class="menu_main" :xs="9" :sm="5" :md="8" :lg="3" :xl="3" >
         <el-row class="menu_title">
-          <el-col @click.native="show = !show" :span="24">
-            <el-col  :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+          <el-col :span="24">
+            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
             <span class="iconfont icon-daohang"></span>
             商品类目
           </el-col>
         </el-row>
-        
-        <el-row @mouseenter.native="expand = true"  v-show="show" class="menu_item" v-for="item in menu_items">
+
+        <el-row @mouseenter.native="expand = true" v-show="show" class="menu_item" v-for="item in menu_items">
           <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
-          <el-col :xs="23" :sm="23" :lg="23" :xl="10" :span="23" >
+          <el-col :xs="23" :sm="23" :lg="23" :xl="10" :span="23">
             <span :class="item.icon"></span>
             <span class="item_word">{{item.name}}</span>
           </el-col>
         </el-row>
       </el-col>
 
-      <el-col v-show="expand" class="menu_show" :xs="17" :sm="18" :md="18" :lg="12" :xl="12" >
+      <el-col v-show="expand" class="menu_show" :xs="17" :sm="18" :md="18" :lg="12" :xl="12">
         <div v-for="kind in menu_show">
           <el-row class="subkind">
             <el-col :xs="0" :sm="0" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
@@ -74,16 +74,16 @@
   color: #ff0036;
 }
 .menu_main {
-  
   color: white;
   min-height: 0;
   max-height: 100%;
   pointer-events: auto;
 }
 .menu_show {
-  background-color: white;
-  max-height: 500px;  
-
+    background-color: white;
+  /* opacity: 1; */
+  height: 100%;
+/* z-index: 1111; */
   pointer-events: auto;
   margin-top: 35px;
 }
@@ -108,7 +108,7 @@ export default {
   data() {
     return {
       expand: false,
-      show:false,
+      show: true,
       menu_items: [
         { icon: "iconfont icon-weibiaoti2fuzhi09", name: "家用电器" },
         { icon: "iconfont icon-diannao", name: "电脑 办公" },
