2c0c8099bcdcf9e2d7876431fe3f843b65a47668
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-19 22:50:06 +0800

    Update product_line.vue

--- src/components/mall/index/product_line.vue
+++ src/components/mall/index/product_line.vue
@@ -1,52 +1,64 @@
 <template>
-    <div>
+  <div>
+    <el-row style="z-index:1;" v-for="products in product_list">
+      <el-row class="product_title">
+        <el-col :xs="24" :sm="8" :md="6" :lg="5" :offset="1" style="font-size:20px;">{{products.title}}</el-col>
+        <el-col :xs="1" :sm="0" :md="0" :lg="3" :xl="4">&nbsp;</el-col>
+        <el-col :xs="6" :sm="3" :md="6" :lg="3">
+          <span class="line_title_more" style="font-size:12px;">更多>></span>
+        </el-col>
+      </el-row>
 
-        <el-row class="product_title">
-            <el-col :span="5" :offset="1" style="font-size:20px;">潮电酷玩 ELECTRONICS</el-col>
-            <el-col :span="3">
-                <span class="line_title_more" style="font-size:12px;">更多>></span>
-            </el-col>
-        </el-row>
-        <el-row style="padding-top:25px;">
-            <el-col :span="4" :offset="3">
-                <div style="position: relative;">
-                    <el-col :class="{'can':display}" @click.native="a_show" :span="24" class="card" @mouseenter.native="show = true " @mouseleave.native="show = false">
-                        <img src="../../../../public/img/product1.jpg" class="image">
-                        <div style="padding: 14px;">
-                            <span>Newsmy纽曼移动硬盘500G</span>
-                            <div class="bottom clearfix">
-                                <time class="time"></time>
-                            </div>
-                            <div style="font-size:12px;color:grey">
-                                纽曼厂商
-                            </div>
-                            <div style="text-align:center; color: rgb(253, 40, 40);">
-                                ￥150
-                            </div>
-                        </div>
-                    </el-col>
-                    <el-col v-show="show" class="show">
-                        <button @click="aaa" class="buttonline">sadsad</button>
-                    </el-col>
+      <el-row style="height:100%;margin-top:15px;margin-bottom:15px">
+
+        <object v-for="lists in products.list">
+          <el-col :xs="24" :sm="6" :md="6" :lg="6" :xl="6" style="height:100%;">
+
+            <div style="position: relative;height:300px;">
+              <el-col :span="24" class="card">
+                <div align="center">
+                  <img :src="lists.img" class="image">
                 </div>
-            </el-col>
-        </el-row>
+                <div style="padding: 14px;">
+                  <span>{{lists.product_name}}</span>
+                  <div style="font-size:12px;color:grey">
+                    {{lists.brand}}
+                  </div>
+                  <div style="text-align:center; color: rgb(253, 40, 40);">
+                    ￥{{lists.price}}
+                  </div>
+                </div>
+              </el-col>
 
-    </div>
+              <el-col class="show">
+                <el-row style="top:50%;">
+                  <el-col :span="2" :offset="6">
+                    <el-button class="buttonline iconfont icon-gouwuche" type="primary" title="添加购物车" circle></el-button>
+                  </el-col>
+                  <el-col :span="2" :offset="3">
+                    <el-button class="buttonline iconfont icon-xihuan" type="danger" title="标记为喜欢" circle></el-button>
+                  </el-col>
+                  <el-col :span="2" :offset="3">
+                    <el-button class="buttonline iconfont icon-xiangqing" type="success" title="查看详情" circle></el-button>
+                  </el-col>
+                </el-row>
+              </el-col>
+
+            </div>
+          </el-col>
+        </object>
+      </el-row>
+    </el-row>
+  </div>
 </template>
 <style scoped>
-.buttonline {
-  top: 50%;
-  opacity: 100%;
-  z-index: 555;
-  position: absolute;
-    pointer-events: auto;
-}
 .product_title {
-  background-color: aquamarine;
+  /* background-color: aquamarine; */
+  border-bottom: 2px solid rgb(253, 40, 40);
   height: auto;
   padding-top: 5px;
   padding-bottom: 5px;
+  margin-top: 50px;
 }
 .line_title_more {
   cursor: pointer;
@@ -55,30 +67,41 @@
   color: rgb(253, 40, 40);
 }
 .image {
-  width: 100%;
+  width: 185px;
+  height: 185px;
+  display: block;
 }
 .show {
   position: absolute;
-  z-index: 100;
-  background-color: #ebeef5;
-  height: 350px;
-  pointer-events: none;
+  z-index: 0;
+
+  height: 100%;
+  width: 100%;
+  pointer-events: auto;
+  /* visibility: visible; */
 }
-
-
+.show:hover {
+  position: absolute;
+  z-index: 100;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  -webkit-transition: all 0.4s ease-in-out;
+  -moz-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  -ms-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+  /* background-color: black; */
+  /* opacity: 0.3; */
+}
 .card {
   border: 1px solid #ebeef5;
+  background-color: white;
   border-radius: 4px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-  height: 350px;
+  height: 300px;
   position: absolute;
   z-index: 1;
-}
-.card:hover {
-  /* pointer-events: auto; */
-}
-.can{
-     pointer-events: none;
+  pointer-events: none;
 }
 </style>
 <script>
@@ -86,17 +109,65 @@ export default {
   data() {
     return {
       show: false,
-      display: false
+      display: false,
+      product_list: [
+        {
+          title: "潮电酷玩 ELECTRONICS",
+          list: [
+            {
+              product_name: "Newsmy纽曼移动硬盘500G",
+              price: "150",
+              brand: "纽曼厂商",
+              img: require("../../../../public/img/product1.jpg")
+            },
+            {
+              product_name: "荣耀 V10",
+              price: "3000",
+              brand: "华为荣耀厂商",
+              img: require("../../../../public/img/product2.jpg")
+            },
+            {
+              product_name: "Apple/苹果 iPhone X",
+              price: "6300",
+              brand: "苹果厂商",
+              img: require("../../../../public/img/product3.jpg")
+            },
+
+            {
+              product_name: "Newsmy纽曼移动硬盘500G",
+              price: "150",
+              brand: "纽曼厂商",
+              img: require("../../../../public/img/product4.jpg")
+            },
+            {
+              product_name: "Samsung/三星 Galaxy S9+ SM-G9650/DS",
+              price: "6400",
+              brand: "三星厂商",
+              img: require("../../../../public/img/product5.jpg")
+            },
+            {
+              product_name: "Apple/苹果 iPhone 8",
+              price: "4058",
+              brand: "苹果厂商",
+              img: require("../../../../public/img/product6.jpg")
+            },
+            {
+              product_name: "vivo Z1青春版",
+              price: "949",
+              brand: "vivo厂商",
+              img: require("../../../../public/img/product7.jpg")
+            },
+            {
+              product_name: "荣耀手机",
+              price: "1500",
+              brand: "华为荣耀",
+              img: require("../../../../public/img/product8.jpg")
+            }
+          ]
+        }
+      ]
     };
   },
-  methods: {
-    a_show(event) {
-      event.stopPropagation();
-    },
-    aaa(event) {
-      alert(2);
-      event.preventDefault();
-    }
-  }
+  methods: {}
 };
 </script>
