fb1aaa720d7adc28c6ad8265a33a3bf27dc06020
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-20 23:07:20 +0800

    Create footer.vue

---
+++ src/components/mall/index/footer.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+        <el-row class="footer_iconline">
+            <el-col :xs="0" :sm="1" :md="1" :lg="3" :xl="1">&nbsp;</el-col>
+            <el-col :span="3"><span class="footer_icon iconfont icon-num-1"></span> 品类齐全，轻松购物</el-col>
+            <el-col :xs="0" :sm="1" :md="1" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col :span="3"><span class="footer_icon iconfont icon-2"></span>多仓直发，极速配送</el-col>
+            <el-col :xs="0" :sm="1" :md="1" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col :span="3"><span class="footer_icon iconfont icon-3"></span>正品行货，精致服务</el-col>
+            <el-col :xs="0" :sm="1" :md="1" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col :span="3"><span class="footer_icon iconfont icon-icon-test1"></span>天天低价，畅选无忧</el-col>
+
+        </el-row>
+
+        <el-row class="footer">
+            <el-col :xs="0" :sm="1" :md="1" :lg="3" :xl="1">&nbsp;</el-col>
+            <el-col class="col" :lg="3">
+                <el-row>
+                    <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                    <el-col :span="23" style="font-size:18px;">购物指南</el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="24">
+                        <ul class="footer_ul">
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>购物流程</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>常见问题</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>免费注册</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>联系客服</li>
+                        </ul>
+                    </el-col>
+
+                </el-row>
+
+            </el-col>
+
+            <el-col :xs="0" :sm="1" :md="1" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col class="col" :lg="3">
+                <el-row>
+                    <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                    <el-col :span="23" style="font-size:18px;">购物指南</el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="24">
+                        <ul class="footer_ul">
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>购物流程</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>常见问题</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>免费注册</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>联系客服</li>
+                        </ul>
+                    </el-col>
+
+                </el-row>
+
+            </el-col>
+
+            <el-col :xs="0" :sm="1" :md="1" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col class="col" :lg="3">
+                <el-row>
+                    <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                    <el-col :span="23" style="font-size:18px;">购物指南</el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="24">
+                        <ul class="footer_ul">
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>购物流程</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>常见问题</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>免费注册</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>联系客服</li>
+                        </ul>
+                    </el-col>
+
+                </el-row>
+
+            </el-col>
+
+            <el-col :xs="0" :sm="1" :md="1" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col class="col" :lg="3">
+                <el-row>
+                    <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                    <el-col :span="23" style="font-size:18px;">购物指南</el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="24">
+                        <ul class="footer_ul">
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>购物流程</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>常见问题</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>免费注册</li>
+                            <el-col :xs="0" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+                            <li>联系客服</li>
+                        </ul>
+                    </el-col>
+
+                </el-row>
+
+            </el-col>
+
+        </el-row>
+        <el-row class="version">
+            <el-col :xs="0" :sm="1" :md="1" :lg="6" :xl="1">&nbsp;</el-col>
+            <el-col :span="6">
+                <span class="iconfont icon-banquan"></span>开发团队：猿聚</el-col>
+            <el-col :span="6">version: 0.1.0</el-col>
+        </el-row>
+
+    </div>
+</template>
+<style scoped>
+.footer_iconline{
+    padding-top: 50px;
+    padding-bottom: 50px;
+}
+.footer_icon{
+    font-size: 30px;
+    color: #ff0036;
+}
+.footer {
+  background-color: #f2f2f2;
+  height: 100%;
+}
+.col {
+  /* background-color: pink; */
+  height: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px; 
+  color: #999;
+}
+.footer_ul {
+  list-style: none;
+  padding-left: 0;
+}
+.footer_ul li {
+  padding-top: 6px;
+}
+.version {
+  /* padding-top: 20px; */
+  background-color: #f2f2f2;
+  height: 40px;
+  line-height: 40px;
+  color: #999;
+  border-top: 1px solid #dedede;
+  text-align: center;
+}
+</style>
+<script>
+export default {};
+</script>
