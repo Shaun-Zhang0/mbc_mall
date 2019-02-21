f1a1b4ab25d847a98d645e065f3dcaa01d481353
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-21 23:02:35 +0800

    Create to_top.vue

---
+++ src/components/mall/index/to_top.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-button class="top" icon="el-icon-arrow-up" circle></el-button>
+</template>
+<style>
+.top {
+  position: fixed;
+  bottom: 20%;
+  right: 10px;
+  z-index: 20000;
+}
+</style>
+<script>
+export default {
+  methods: {
+    top() {
+    
+      
+    }
+  }
+};
+</script>
