5e0f8b3581f3121e05ef496f7364d72229bec8e9
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-22 16:20:37 +0800

    Update to_top.vue

--- src/components/mall/index/to_top.vue
+++ src/components/mall/index/to_top.vue
@@ -1,5 +1,7 @@
 <template>
-    <el-button class="top" icon="el-icon-arrow-up" circle></el-button>
+  <el-tooltip  effect="dark" content="返回顶部" placement="bottom-start">
+    <el-button class="top" icon="el-icon-arrow-up" :style="{display : display}" @click="action_top" circle></el-button>
+  </el-tooltip>
 </template>
 <style>
 .top {
@@ -8,13 +10,35 @@
   right: 10px;
   z-index: 20000;
 }
+
 </style>
 <script>
 export default {
+  data() {
+    return {
+      top_height: null,
+      display: ""
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll, true);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll); // 离开页面 关闭监听 不然会报错
+  },
   methods: {
-    top() {
-    
-      
+    // 获取滚动条高度
+    handleScroll() {
+      this.top_height = window.scrollY;
+      if (this.top_height < 400) {
+        this.display = "none";
+      } else {
+        this.display = "block";
+      }
+    },
+    action_top() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
     }
   }
 };
