e265dd653b50c0e0d0f765b82e4118fc2184e4a4
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-02-25 15:38:48 +0800

    Update search.vue

--- src/components/mall/index/search.vue
+++ src/components/mall/index/search.vue
@@ -2,39 +2,41 @@
  * @Author: Shaun.Zhang 
  * @Date: 2019-02-14 17:01:58 
  * @Last Modified by: Shaun.Zhang
- * @Last Modified time: 2019-02-22 23:06:12
+ * @Last Modified time: 2019-02-23 15:38:29
  */
 <template>
-    <div>
-        <el-row class="row">
-            <el-col :xs="8" :sm="10" :md="10" :lg="1" :xl="1">&nbsp;</el-col>
-            <el-col :xs="16" :sm="14" :md="14" :lg="4" style="height:100%">
-                <span style="font-size: 30px;color: rgb(253, 40, 40);">跨境商城</span>
-            </el-col>
-            <el-col :xs="2" :sm="7" :md="7" :lg="3" :xl="3">&nbsp;</el-col>
-            <el-col :xs="20" :sm="10" :md="10" :lg="8" :xl="9" class="form">
-                <el-row>
-                    <el-input class="input search_input" placeholder="搜索 商品/厂商">
-                        <el-button class="el-icon-search" style="background-color:rgb(253, 40, 40);color:white;" slot="append">搜索</el-button>
-                    </el-input>
-                </el-row>
-                <el-row style="margin-top:-20px;">
-                    <el-col :xs="5" :sm="4" :md="4" :lg="3" :xl="3" class="suggest" v-for="words in suggest_word">
-                        <span class="suggest_item" style="">{{words.word}}</span>
-                    </el-col>
-                </el-row>
-            </el-col>
+  <div>
+    <el-row class="row">
+      <el-col :xs="8" :sm="10" :md="10" :lg="1" :xl="1">&nbsp;</el-col>
+      <el-col :xs="16" :sm="14" :md="14" :lg="4" style="height:100%">
+        <router-link to="/mall/index" tag="span">
+          <span style="font-size: 30px;color: rgb(253, 40, 40);cursor:pointer;">跨境商城</span>
+        </router-link>
+      </el-col>
+      <el-col :xs="2" :sm="7" :md="7" :lg="3" :xl="3">&nbsp;</el-col>
+      <el-col :xs="20" :sm="10" :md="10" :lg="8" :xl="9" class="form">
+        <el-row>
+          <el-input class="input search_input" placeholder="搜索 商品/厂商">
+            <el-button class="el-icon-search" style="background-color:rgb(253, 40, 40);color:white;" slot="append">搜索</el-button>
+          </el-input>
         </el-row>
-    </div>
+        <el-row style="margin-top:-20px;">
+          <el-col :xs="5" :sm="4" :md="4" :lg="3" :xl="3" class="suggest" v-for="words in suggest_word">
+            <span class="suggest_item" style="">{{words.word}}</span>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <style >
 .row {
   max-height: 150px;
   line-height: 100px;
   width: 100%;
-  margin-bottom: 5px;
+  margin-bottom: 80px;
 }
-.form { 
+.form {
   height: 100%;
   line-height: 90px;
 }
@@ -50,7 +52,7 @@
   font-size: 12px;
 }
 .el-input-group__append {
-  background-color: rgb(253, 40, 40)!important ;
+  background-color: rgb(253, 40, 40) !important ;
   border: none !important;
   border-radius: 0px;
 }
