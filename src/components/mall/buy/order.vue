28ff5fcf46da4f876dd1e7476217346a864cf97c
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-01 18:59:44 +0800

    Update order.vue

--- src/components/mall/buy/order.vue
+++ src/components/mall/buy/order.vue
@@ -21,72 +21,117 @@
                         </el-steps>
                     </el-col>
                 </el-row>
-                <el-row v-show="active == 0" style="height:300px;margin-top:10px;">
-                    <el-row style="font-size:2.5vh;height:50px;line-height:50px;margin: 0% 3% 0%;border-bottom:2px solid #ccc;">
-                        <el-col :span="12" :offset="2">
-                            <span>确认订单信息 </span>
+                <el-row v-show="active == 0 || active == 1">
+                    <el-row style="height:300px;margin-top:10px;">
+                        <el-row style="font-size:2.5vh;height:50px;line-height:50px;margin: 0% 3% 0%;border-bottom:2px solid #ccc;">
+                            <el-col :span="12" :offset="2">
+                                <span>确认订单信息 </span>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-row class="order_info" style="height:150px;margin:0% 3% 0%;border-bottom: 2px solid #ccc;">
+                                <el-table :data="order_data" style="width: 100%;">
+
+                                    <el-table-column prop="order_num" label="订单号" width="180">
+                                    </el-table-column>
+
+                                    <el-table-column prop="img_url" label="商品图片" width="180">
+                                        <template slot-scope="scope">
+                                            <img :src="scope.row.img_url" style="width: 50px;height: 50px;">
+                                        </template>
+                                    </el-table-column>
+
+                                    <el-table-column prop="product_name" label="商品名称" width="200">
+                                    </el-table-column>
+
+                                    <el-table-column prop="product_brand" label="厂商名称" width="180">
+                                    </el-table-column>
+
+                                    <el-table-column prop="product_color" label="颜色" width="180">
+                                    </el-table-column>
+                                    <el-table-column prop="product_num" label="数量" width="200">
+                                        <template slot-scope="scope">
+                                            <el-input-number @change="{{order_sumprice}}" size="mini" v-model="scope.row.product_num" :min="1" :max="5"></el-input-number>
+                                        </template>
+                                    </el-table-column>
+
+                                    <el-table-column prop="product_price" label="单价(元)" width="180">
+                                    </el-table-column>
+
+                                    <el-table-column prop="order_price" label="订单金额(元)">
+                                    </el-table-column>
+
+                                </el-table>
+                            </el-row>
+                        </el-row>
+                    </el-row>
+                    <el-row style="text-align:right;">
+                        <el-col :span="20" :offset="2">
+                            <el-button @click="confirmOrder">确认订单信息</el-button>
+                            <!-- <el-button v-show="active == 1" @click="confirmOrder">支付</el-button> -->
                         </el-col>
                     </el-row>
-                    <el-row>
-                        <el-row class="order_info" style="height:150px;margin:0% 3% 0%;border-bottom: 2px solid #ccc;">
-                            <el-table :data="order_data" style="width: 100%;">
-
-                                <el-table-column prop="order_num" label="订单号" width="180">
-                                </el-table-column>
-
-                                <el-table-column prop="img_url" label="商品图片" width="180">
-                                    <template slot-scope="scope">
-                                        <img :src="scope.row.img_url" style="width: 50px;height: 50px;">
-                                    </template>
-                                </el-table-column>
-
-                                <el-table-column prop="product_name" label="商品名称" width="200">
-                                </el-table-column>
-
-                                <el-table-column prop="product_brand" label="厂商名称" width="180">
-                                </el-table-column>
-
-                                <el-table-column prop="product_color" label="颜色" width="180">
-                                </el-table-column>
-                                <el-table-column prop="product_num" label="数量" width="200">
-                                    <template slot-scope="scope">
-                                        <el-input-number @change="{{order_sumprice}}" size="mini" v-model="scope.row.product_num" :min="1" :max="5"></el-input-number>
-                                    </template>
-                                </el-table-column>
-
-                                <el-table-column prop="product_price" label="单价(元)" width="180">
-                                </el-table-column>
-
-                                <el-table-column prop="order_price" label="订单金额(元)">
-                                </el-table-column>
-
-                            </el-table>
+                </el-row>
+                <el-row v-show="active == 2">
+                    <el-row style="height:300px;margin-top:10px;">
+                        <el-row style="font-size:2.5vh;height:50px;line-height:50px;margin: 0% 3% 0%;border-bottom:2px solid #ccc;">
+                            <el-col :span="12" :offset="2">
+                                <span>确认支付 </span>
+                            </el-col>
                         </el-row>
+                        <el-row>
+                            <el-col :span="24" style="text-align:center;height:300px;line-height:300px;">
+                                <span style="font-size:50px;" :style="{color:status}" :class="payment_icon"></span>
+                                <span :style="{color:status}">{{paystatus}}</span>
+                            </el-col>
+                            <el-col :span="12"></el-col>
+                        </el-row>
+
                     </el-row>
 
                 </el-row>
-                <el-row v-show="active == 1" style="height:300px;margin-top:10px;">
-                    <el-row style="font-size:2.5vh;height:50px;line-height:50px;margin: 0% 3% 0%;border-bottom:2px solid #ccc;">
-                        <el-col :span="12" :offset="2">
-                            <span>确认支付</span>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-row class="order_info" style="height:150px;margin:0% 3% 0%;border-bottom: 2px solid #ccc;">
-                           
+                <el-row v-show="active == 3">
+                    <el-row style="height:300px;margin-top:10px;">
+                        <el-row style="font-size:2.5vh;height:50px;line-height:50px;margin: 0% 3% 0%;border-bottom:2px solid #ccc;">
+                            <el-col :span="12" :offset="2">
+                                <span>购买成功 </span>
+                            </el-col>
                         </el-row>
+                        <el-row>
+                            <el-col :span="24" style="text-align:center;height:300px;line-height:300px;">
+                                <span><el-button>{{totalTime}} 秒后返回主页</el-button></span>
+                            </el-col>
+                            <el-col :span="12"></el-col>
+                        </el-row>
+
                     </el-row>
 
                 </el-row>
+            </el-row>
+            <el-dialog title="订单支付" :visible.sync="dialogpayment" width="350px">
+                <el-row>
+                    <el-form label-width="90px">
+                        <el-form-item label="订单号：">
+                            {{order_data[0].order_num}}
+                        </el-form-item>
+                        <el-form-item label="订单金额：">
+                            {{order_data[0].order_price}} 元
+                        </el-form-item>
+                        <el-form-item label="创建时间：">
+                            {{order_data[0].order_createtime}}
+                        </el-form-item>
 
-                <el-row style="text-align:right;">
-                    <el-col :span="20" :offset="2">
-                        <el-button @click="active++">确认订单信息</el-button>
+                    </el-form>
+                </el-row>
+                <el-row>
+
+                    <el-col :span="24">
+                        <el-button style="width:100%;" type="primary" @click="payment">确 定 支 付</el-button>
                     </el-col>
 
                 </el-row>
 
-            </el-row>
+            </el-dialog>
         </div>
         <index-footer></index-footer>
     </div>
@@ -131,27 +176,52 @@ export default {
       var total_price = num * price;
       this.order_data[0].order_price = total_price;
     }
-
-    //   console.log(this.order_data[0].product_num);
   },
   data() {
     return {
-      active: 0,
-
+      active: 3,
+      totalTime: 5,
+      paystatus: "正在支付中",
+      payment_icon: "el-icon-loading",
+      dialogpayment: false,
+      status: "black",
       order_data: [
         {
           order_num: "1610154541",
+          order_createtime: "2019-12-06 12:00:59",
           product_name: "IPHONE X 64G 全网通4G智能手机",
           product_brand: "APPLE/苹果",
           product_num: 1,
           product_price: 6400,
           product_color: "红色",
           order_price: 6400,
-          img_url: require("./../../../../public/img/product3.jpg"),
-          address: "上海市普陀区金沙江路 1518 弄"
+          img_url: require("./../../../../public/img/product3.jpg")
         }
       ]
     };
+  },
+  methods: {
+    confirmOrder() {
+      this.active = 1;
+      this.dialogpayment = true;
+    },
+    payment() {
+      this.active = 2;
+      this.dialogpayment = false;
+      setTimeout(() => {
+        this.paystatus = "支付完成";
+        this.payment_icon = "el-icon-check";
+        this.status = "#67c23a";
+        this.active = 3;
+      }, 3000);
+    },
+    countDown() {
+    let clock = window.setInterval(() => {
+      this.total--
+      this.content = this.total + 's后重新发送'
+    },1000)
+  }
+    
   }
 };
 </script>
