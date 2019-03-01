590a30363f4963372ad6f81e8f0ad645518cd7bd
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-01 16:50:38 +0800

    Update product_line.vue

--- src/components/mall/index/product_line.vue
+++ src/components/mall/index/product_line.vue
@@ -1,3 +1,10 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-02-28 17:55:13 
+ * @Last Modified by:   Shaun.Zhang 
+ * @Last Modified time: 2019-02-28 17:55:13 
+ */
+
 <template>
   <div>
     <el-row style="z-index:1;" v-for="products in product_list">
@@ -39,7 +46,7 @@
                   </el-col>
                   <el-col :span="2" :offset="3">
                     <router-link :to="'/mall/product/'+lists.id" tag="span">
-                    <!-- <router-link :to="'/mall/product/'+lists.id" tag="span"> -->
+                      <!-- <router-link :to="'/mall/product/'+lists.id" tag="span"> -->
                       <el-button class="buttonline iconfont icon-xiangqing" type="success" title="查看详情" circle></el-button>
                     </router-link>
                   </el-col>
@@ -120,51 +127,58 @@ export default {
               product_name: "Newsmy纽曼移动硬盘500G",
               price: "150",
               brand: "纽曼厂商",
-              img: require("../../../../public/img/product1.jpg")
+              img: require("../../../../public/img/product1.jpg"),
+              id: "1"
             },
             {
               product_name: "荣耀 V10",
               price: "3000",
               brand: "华为荣耀厂商",
-              img: require("../../../../public/img/product2.jpg")
+              img: require("../../../../public/img/product2.jpg"),
+              id: "2"
             },
             {
               product_name: "Apple/苹果 iPhone X",
               price: "6300",
               brand: "苹果厂商",
               img: require("../../../../public/img/product3.jpg"),
-              id: "123456"
+              id: "3"
             },
 
             {
               product_name: "Newsmy纽曼移动硬盘500G",
               price: "150",
               brand: "纽曼厂商",
-              img: require("../../../../public/img/product4.jpg")
+              img: require("../../../../public/img/product4.jpg"),
+              id: "4"
             },
             {
               product_name: "Samsung/三星 Galaxy S9+ SM-G9650/DS",
               price: "6400",
               brand: "三星厂商",
-              img: require("../../../../public/img/product5.jpg")
+              img: require("../../../../public/img/product5.jpg"),
+              id: "5"
             },
             {
               product_name: "Apple/苹果 iPhone 8",
               price: "4058",
               brand: "苹果厂商",
-              img: require("../../../../public/img/product6.jpg")
+              img: require("../../../../public/img/product6.jpg"),
+              id: "6"
             },
             {
               product_name: "vivo Z1青春版",
               price: "949",
               brand: "vivo厂商",
-              img: require("../../../../public/img/product7.jpg")
+              img: require("../../../../public/img/product7.jpg"),
+              id: "7"
             },
             {
               product_name: "荣耀手机",
               price: "1500",
               brand: "华为荣耀",
-              img: require("../../../../public/img/product8.jpg")
+              img: require("../../../../public/img/product8.jpg"),
+              id: "8"
             }
           ]
         },
@@ -176,20 +190,23 @@ export default {
                 "荣事达磨粉机家用小型打粉机五谷杂粮研磨机电动干磨机药材粉碎机 ",
               price: "159",
               brand: "荣事达厂商",
-              img: require("../../../../public/img/product2-1.jpg")
+              img: require("../../../../public/img/product2-1.jpg"),
+              id: "9"
             },
             {
               product_name: "苏泊尔电热水壶 SW-17D618 双层不锈钢防烫保温1.7L升",
               price: "109",
               brand: "苏泊尔厂商",
-              img: require("../../../../public/img/product2-2.jpg")
+              img: require("../../../../public/img/product2-2.jpg"),
+              id: "10"
             },
             {
               product_name:
                 "小熊煎蛋器JDQ-C3011 电煎锅迷你自动断电煮蒸蛋羹宿舍煎蛋机",
               price: "78",
               brand: "小熊厂商",
-              img: require("../../../../public/img/product2-3.jpg")
+              img: require("../../../../public/img/product2-3.jpg"),
+              id: "11"
             },
 
             {
@@ -197,34 +214,39 @@ export default {
                 "净水器家用直饮厨房自来水过滤器前置五级除垢超滤净水机",
               price: "200",
               brand: "志高厂商",
-              img: require("../../../../public/img/product2-4.jpg")
+              img: require("../../../../public/img/product2-4.jpg"),
+              id: "12"
             },
             {
               product_name:
                 "美的饮水机设备MT-3 865CB MC-3 865CB净水桶过滤芯耗材配件滤网",
               price: "61",
               brand: "美的厂商",
-              img: require("../../../../public/img/product2-5.jpg")
+              img: require("../../../../public/img/product2-5.jpg"),
+              id: "13"
             },
             {
               product_name:
                 "奥克斯绞肉机家用电动小型绞馅机切菜打肉机蒜泥蒜蓉碎菜碎肉机",
               price: "119",
               brand: "奥克斯厂商",
-              img: require("../../../../public/img/product2-6.jpg")
+              img: require("../../../../public/img/product2-6.jpg"),
+              id: "14"
             },
             {
               product_name:
                 "海尔商用净水机HZR400-2W智能反渗透饮水机加热型商务净饮机办公",
               price: "17200",
               brand: "海尔厂商",
-              img: require("../../../../public/img/product2-7.jpg")
+              img: require("../../../../public/img/product2-7.jpg"),
+               id: "15"
             },
             {
               product_name: "美的净水器家用直饮自来水过滤器反渗透RO净水机",
               price: "1898",
               brand: "华为荣耀",
-              img: require("../../../../public/img/product2-8.jpg")
+              img: require("../../../../public/img/product2-8.jpg"),
+               id: ""
             }
           ]
         }
