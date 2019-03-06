e45657e5b7c0ac0ca2a84854fcc5b35077880862
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-06 23:08:34 +0800

    Create cookie.js

---
+++ src/assets/js/cookie.js
@@ -0,0 +1,35 @@
+const setCookie = function setCookie(name,value) 
+{ 
+    var Days = 30; 
+    var exp = new Date(); 
+    exp.setTime(exp.getTime() + Days*24*60*60*1000); 
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
+} 
+
+//读取cookies 
+const getCookie = function getCookie(name) 
+{ 
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+ 
+    if(arr=document.cookie.match(reg))
+ 
+        return unescape(arr[2]); 
+    else 
+        return null; 
+} 
+
+//删除cookies 
+const delCookie = function delCookie(name) 
+{ 
+    var exp = new Date(); 
+    exp.setTime(exp.getTime() - 1); 
+    var cval=getCookie(name); 
+    if(cval!=null) 
+        document.cookie= name + "="+cval+";expires="+exp.toGMTString(); 
+} 
+export default{
+    setCookie,getCookie,delCookie
+}
+//使用示例 
+// setCookie("name","hayden"); 
+// alert(getCookie("name")); 
