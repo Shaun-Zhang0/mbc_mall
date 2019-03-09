e200fd34b0fa9244e2e8158ae3b3d08a6b96288c
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-09 21:24:02 +0800

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,12 @@
 "use strict";
 
-import Vue from 'vue';
+import Vue from "vue";
 import axios from "axios";
+import router from "../router";
+import { message } from "./element";
+import { Message } from "element-ui";
+import Cookie from "../assets/js/cookie"; //设置cookie
+
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -14,11 +19,35 @@ let config = {
   // withCredentials: true, // Check cross-site Access-Control
 };
 
+Vue.use(router);
+
 const _axios = axios.create(config);
+
 
 _axios.interceptors.request.use(
   function(config) {
     // Do something before request is sent
+    console.log(config.url);
+    // if(config.url == "http://localhost:9000/api/adminLogin/login"){
+    //   var myInterceptor = axios.interceptors.request.use(function () {
+    //     console.log(123);
+    //   });
+    //   axios.interceptors.request.eject(myInterceptor);
+    //   // axios.interceptors.request.eject;
+   
+    // }
+
+
+    
+    // console.log(Cookie.getCookie("token")); 
+    if(!Cookie.getCookie("token") && config.url != "http://localhost:9000/api/adminLogin/login"){
+      Message.error({
+        message: "登录验证已过期，请重新登录"
+      });
+      router.push({ path: "/brand/login" }); /**路由跳转到系统首页 */
+    
+    
+    }
     return config;
   },
   function(error) {
@@ -35,18 +64,34 @@ _axios.interceptors.response.use(
   },
   function(error) {
     // Do something with response error
-    switch(error.response.status)
-    {
-    case 401:
-      console.log("登录验证已过期，请重新登录");
-      break;
-    case 404:
-      console.log("服务器找不到相关的资源");
-      break;
-    case 500:
-      console.log("服务器出错，请稍后再试");
-      break;
-    
+    switch (error.response.status) {
+      case 401:
+        console.log("登录验证已过期，请重新登录");
+        this.$router.push({ path: "/brand/login" }); /**路由跳转到系统首页 */
+        Message.error({
+          message: "服务器出错，请稍后再试"
+        });
+        break;
+      case 404:
+        console.log("服务器找不到相关的资源");
+        Message.error({
+          message: "服务器找不到相关的资源"
+        });
+        break;
+      case 500:
+        console.log("服务器出错，请稍后再试");
+        // router.push({ path: "/brand/login" }); /**路由跳转到系统首页 */
+        Message.error({
+          message: "服务器出错，请稍后再试"
+        });
+        break;
+      case 504:
+        console.log("服务器超时，请刷新页面重试");
+        // router.push({ path: "/brand/login" }); /**路由跳转到系统首页 */
+        Message.error({
+          message: "服务器超时，请刷新页面重试"
+        });
+        break;
     }
     return Promise.reject(error);
   }
@@ -65,10 +110,10 @@ Plugin.install = function(Vue, options) {
       get() {
         return _axios;
       }
-    },
+    }
   });
 };
 
-Vue.use(Plugin)
+Vue.use(Plugin);
 
 export default Plugin;
