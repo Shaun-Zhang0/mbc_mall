a5942f28d156b2ff2016289ffb0835c7bed4d6c8
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-14 23:14:32 +0800

    Create global_ data.vue

---
+++ src/components/global_ data.vue
@@ -0,0 +1,13 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-03-12 13:13:37 
+ * @Last Modified by: Shaun.Zhang
+ * @Last Modified time: 2019-03-12 13:22:59
+ */
+
+<script>
+var  search_Keyword = "";
+export default {
+    search_Keyword
+}
+</script>
