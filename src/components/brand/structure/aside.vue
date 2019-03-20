9f1067a84c22570d5fa856ab1eab546be1783eb0
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:01 +0800

    Update aside.vue

--- src/components/brand/structure/aside.vue
+++ src/components/brand/structure/aside.vue
@@ -2,7 +2,7 @@
  * @Author: Shaun.Zhang 
  * @Date: 2019-01-25 16:41:27 
  * @Last Modified by: Shaun.Zhang
- * @Last Modified time: 2019-02-12 17:54:38
+ * @Last Modified time: 2019-03-20 14:45:52
  */
 
 <template>
@@ -40,7 +40,7 @@
                             <el-menu-item-group>
                               
                                 <router-link to="/brand/orderlist" tag="li"><el-menu-item index="3-1">用户订单列表</el-menu-item></router-link>
-                                <router-link to="/brand/Chargerback" tag="li"><el-menu-item index="3-2">用户退单列表</el-menu-item></router-link>
+                                <el-menu-item index="3-2">用户退单列表</el-menu-item>
                                 
                             </el-menu-item-group>
                         
@@ -53,7 +53,7 @@
                           
                             <el-menu-item-group>
                               
-                                <router-link to="/brand/edit_order" tag="li"><el-menu-item index="4-1">用户订单列表</el-menu-item></router-link>
+                                <el-menu-item index="4-1">用户评论列表</el-menu-item>
                                 
                             </el-menu-item-group>
                         
@@ -67,11 +67,23 @@
                                 <el-menu-item index="5-1">查询订单物流</el-menu-item>
                             </el-menu-item-group>
                         </el-submenu>
-                        <el-menu-item index="6">
+                        
+                        <el-submenu index="6">
+                            <template slot="title">
+                                <i class="iconfont icon-zhangdan" style="font-size: 18px;margin-right: 10px;"></i>
+                                <span>财务管理</span>
+                            </template>
+                            <router-link to="/brand/finance" tag="li"> 
+                            <el-menu-item-group>
+                                <el-menu-item index="6-1">财务管理</el-menu-item>
+                            </el-menu-item-group>
+                            </router-link>
+                        </el-submenu>
+                        <el-menu-item index="7">
                                 <i class="iconfont icon-cangku2" style="font-size: 18px;margin-right: 10px;"></i>
                                 <span slot="title">仓库管理</span>
                             </el-menu-item>
-                        <el-menu-item index="7">
+                        <el-menu-item index="8">
                             <i class="iconfont icon-linechart2" style="font-size: 18px;margin-right: 10px;"></i>
                             <span slot="title">销售统计</span>
                         </el-menu-item>
