d35fc51b5ef17c1edf5acdb97829821a9b51bc7e
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:39 +0800

    Update product.vue

--- src/components/mall/product/product.vue
+++ src/components/mall/product/product.vue
@@ -2,115 +2,132 @@
  * @Author: Shaun.Zhang 
  * @Date: 2019-02-28 17:55:08 
  * @Last Modified by: Shaun.Zhang
- * @Last Modified time: 2019-03-12 11:18:49
+ * @Last Modified time: 2019-03-20 20:45:47
  */
 
 <template>
+  <div>
     <div>
-        <div>
-            <index-header></index-header>
-        </div>
-        <div>
-            <index-search></index-search>
-        </div>
-        <el-row class="product_all">
-            <el-col :xs="0" :sm="2" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
-            <el-col class="img_all" :md="24" :lg="15">
-                <img class="img" :src="img_src">
-            </el-col>
-            <el-col :md="24" :lg="8">
-                <el-row class="product_title">
-                    <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
-                    <el-col :md="24" :lg="20">
-                        <el-col :xs="2" :sm="8" :md="8" :lg="0" :xl="0">&nbsp;</el-col>
-                        <span> {{product.product_name}}</span>
-                    </el-col>
-                </el-row>
-                <el-row class="product_price">
-                    <el-col :md="24" :lg="20">
-                        <span>￥6300</span>
-                    </el-col>
-                </el-row>
+      <index-header></index-header>
+    </div>
+    <div>
+      <index-search></index-search>
+    </div>
+    <el-row class="product_all">
+      <el-col :xs="0" :sm="2" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+      <el-col class="img_all" :md="24" :lg="15">
+        <img class="img" :src="product.img_src">
+      </el-col>
+      <el-col :md="24" :lg="8">
+        <el-row class="product_title">
+          <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
+          <el-col :md="24" :lg="20">
+            <el-col :xs="2" :sm="8" :md="8" :lg="0" :xl="0">&nbsp;</el-col>
+            <span> {{product.name}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="product_price">
+          <el-col :md="24" :lg="20">
+            <span>￥{{product.price}}</span>
+          </el-col>
+        </el-row>
 
-                <el-row class="product_brand">
-                    <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
-                    <el-col :md="24" :lg="20">
-                        所属厂商
-                        <span>{{product.product_brand}}</span>
-                    </el-col>
-                </el-row>
+        <el-row class="product_brand">
+          <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
+          <el-col :md="24" :lg="20">
+            所属厂商
+            <span>{{product.product_brand}}</span>
+          </el-col>
+        </el-row>
 
-                <el-row class="product_sell">
-                    <el-col :xs="8" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
-                    <el-col :md="24" :lg="18">
-                        累计售出
-                        <span>{{product.product_sell}}</span>
-                    </el-col>
-                </el-row>
+        <el-row class="product_brand">
+          <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
+          <el-col :md="24" :lg="20">
+            商品状态
+            <span>{{product.product_status}}</span>
+          </el-col>
+        </el-row>
 
-                <!-- <el-row class="product_color">
-                    <el-row>
-                        <el-col :xs="8" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
-                        <el-col :md="24" :lg="20">颜色分类</el-col>
-                    </el-row>
-                    <el-row class="color_button">
-                        <el-col :xs="0" :sm="7" :md="7" :lg="2" :xl="1">&nbsp;</el-col>
-                        <el-col :xs="24" :sm="12" :md="20" :lg="20">
-                            <el-col v-for="color in product.product_color" :md="6" :span="6">
-                                <el-radio @change="select_color" v-model="buy_color" :label="color.color" border>{{color.color}}</el-radio>
-                            </el-col>
+        <el-row class="product_sell">
+          <el-col :xs="8" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
+          <el-col :md="24" :lg="18">
+            累计售出
+            <span>{{product.product_sell}}</span>
+          </el-col>
+        </el-row>
 
-                        </el-col>
-                    </el-row>
-                </el-row> -->
-
-                <el-row class="buy_num">
-                    <el-row >
-                        <el-col :xs="0" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
-                        <el-col :xs="10 " :md="4" :lg="4">选择数量</el-col>
-                        <!-- <el-col :xs="10" :md="8" :lg="9" class="stock" :style="{display : stock}">{{buy_color}}剩余库存:
+        <el-row class="buy_num">
+          <el-row>
+            <el-col :xs="0" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col :xs="10 " :md="4" :lg="4">选择数量</el-col>
+            <!-- <el-col :xs="10" :md="8" :lg="9" class="stock" :style="{display : stock}">{{buy_color}}剩余库存:
                             <span>11</span>
                         </el-col> -->
-                    </el-row>
-                    <el-row>
-                        <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
-                        <el-col class="select_num" :sm="4" :md="4" :lg="20">
-                            <el-input-number :title='"最大限购数量为："+product.buy_limit' v-model="select_num" :min="1" :max="product.buy_limit" label="描述文字"></el-input-number>
-                        </el-col>
-                    </el-row>
-                </el-row>
-
-                <el-row class="confirm">
-                    <el-col :xs="8" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
-                    <el-col :md="24" :lg="20">
-                            <el-button type="info" class="confirm_button">确认代理</el-button>
-                    </el-col>
-                </el-row>
-
-                <el-row class="product_details">
-                    <el-row class="details_tip" >
-                        <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
-                        <el-col :span="20">
-                            <span>查看更多手机</span>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :xs="8" :sm="10" :md="5" :lg="2" :xl="1">&nbsp;</el-col>
-                        <el-col :md="14" :lg="18">
-                            <span>{{product.product_description}}</span>
-                        </el-col>
-                    </el-row>
-                </el-row>
+          </el-row>
+          <el-row>
+            <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col class="select_num" :sm="4" :md="4" :lg="20">
+              <el-input-number :title='"最大限购数量为："+product.buy_limit' v-model="select_num" :min="1" :max="product.buy_limit" label="描述文字"></el-input-number>
             </el-col>
-
+          </el-row>
         </el-row>
-        <div class="footer">
-            <IndexFooter></IndexFooter>
-        </div>
-        <div>
-            <MallLogin></MallLogin>
-        </div>
+
+        <el-row class="confirm">
+          <el-col :xs="8" :sm="10" :md="2" :lg="2" :xl="1">&nbsp;</el-col>
+          <el-col :md="24" :lg="20">
+            <el-button type="info" class="confirm_button" @click="checkLogin">确认代理</el-button>
+          </el-col>
+        </el-row>
+
+        <el-row class="product_details">
+          <el-row class="details_tip">
+            <el-col :xs="8" :sm="10" :md="10" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col :span="20">
+              <span>查看更多商品</span>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :xs="8" :sm="10" :md="5" :lg="2" :xl="1">&nbsp;</el-col>
+            <el-col :md="14" :lg="18">
+              <span>{{product.description}}</span>
+            </el-col>
+          </el-row>
+        </el-row>
+      </el-col>
+
+    </el-row>
+    <div class="footer">
+      <IndexFooter></IndexFooter>
     </div>
+    <div>
+      <MallLogin></MallLogin>
+    </div>
+    <el-dialog :before-close="quitDialog" title="确认代理" :visible.sync="dialogCheckProduct" width="400px">
+      <el-row>
+        <el-col :span="12">商品名称：{{product.name}}</el-col>
+        <el-col :span="12">所属厂商：{{product.product_brand}}</el-col>
+      </el-row>
+      <el-row style="margin-top: 20px">
+        <el-col :span="12">代理数量：{{select_num}}</el-col>
+      </el-row>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="back">返 回</el-button>
+        <el-button type="primary" @click="proxyProduct" :disabled="proxySuccess">
+          <span v-show="!proxystatus&&!proxySuccess">确认代理</span>
+          <span v-show="proxystatus&&!proxySuccess">代理中
+            <span class="el-icon-loading"></span>
+          </span>
+
+          <span v-show="proxystatus&&proxySuccess">代理成功
+            <span class="el-icon-check"></span>
+          </span>
+        </el-button>
+      </span>
+    </el-dialog>
+    </el-dialog>
+
+  </div>
 </template>
 <style scoped>
 .product_all {
@@ -198,6 +215,8 @@ import IndexHeader from "./../index/header.vue";
 import IndexSearch from "./../index/search";
 import IndexFooter from "./../index/footer";
 import MallLogin from "./../login/login";
+import { apiMallProductshow } from "./../../../assets/js/axios/api.js";
+import { apiProxyProduct } from "./../../../assets/js/axios/api.js";
 export default {
   components: {
     IndexHeader: IndexHeader,
@@ -207,32 +226,84 @@ export default {
   },
   created() {
     window.scrollTo(0, 0);
+    console.log(this.$route.params.id);
+    this.productInit();
   },
   data() {
     return {
-      img_src: require("./../../../../public/img/product3.jpg"),
+      proxystatus: false, //正在代理
+      proxySuccess: false, //是否代理成功
+      dialogCheckProduct: false, //确认代理信息对话框
       select_num: 1,
       buy_color: "",
       stock: "none",
+      productId: "",
       product: {
-        product_color: [
-          { color: "红色" },
-          { color: "黑色" },
-          { color: "黄色" },
-          { color: "蓝色" }
-        ],
-        product_name: "IPHONE X 64G 全网通4G智能手机",
+        img_src: require("./../../../../public/img/product3.jpg"),
+        product_status: 1,
+        name: "IPHONE X 64G 全网通4G智能手机",
         product_brand: "APPLE/苹果",
         product_sell: 11,
-        product_description:
+        price: 6300,
+        description:
           "iPhone 上最大的超视网膜显示屏，性能出类拔萃的 A12 仿生，安全性更进一步的面容 ID，以及支持景深控制的突破性双镜头系统。",
         buy_limit: 5
       }
     };
   },
   methods: {
-    select_color() {
-      this.stock = "block";
+    productInit() {
+      apiMallProductshow(this.$route.params.id).then(res => {
+        console.log(res.data.data);
+
+        if (res.data.code == 4000) {
+          this.$message({
+            type: "warning",
+            message: "该商品不存在 "
+          });
+        } else if (res.data.code == 200) {
+          this.product = res.data.data;
+          this.productId = res.data.data.id;
+          this.product.img_src = res.data.data.picture;
+        }
+      });
+    },
+    checkLogin() {
+      if (!this.Cookie.getCookie("storeToken")) {
+        this.$message({
+          type: "warning",
+          message: "您还没登录，请登录后在进行操作"
+        });
+
+      } else {
+        this.dialogCheckProduct = true;
+      }
+    },
+    quitDialog(done) {
+      this.dialogCheckProduct = false;
+      this.proxystatus = false;
+      this.proxySuccess = false;
+      done();
+    },
+    back() {
+      this.dialogCheckProduct = false;
+      this.proxystatus = false;
+      this.proxySuccess = false;
+    },
+    proxyProduct() {
+      // this.dialogCheckProduct = false;
+
+      this.proxystatus = true;
+      apiProxyProduct({
+        productId: this.productId,
+        storeStock: this.select_num
+      }).then(res => {
+        console.log(res.data);
+        if (res.data.code == 200) {
+          // this.dialogCheckProduct = false;
+          this.proxySuccess = true;
+        }
+      });
     }
   }
 };
