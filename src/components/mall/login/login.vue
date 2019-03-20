facd29374bb71b00b14a3b7850c023386f8ea6af
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:31 +0800

    Update login.vue

--- src/components/mall/login/login.vue
+++ src/components/mall/login/login.vue
@@ -2,23 +2,23 @@
  * @Author: Shaun.Zhang 
  * @Date: 2019-02-25 16:15:05 
  * @Last Modified by: Shaun.Zhang
- * @Last Modified time: 2019-03-02 19:42:13
+ * @Last Modified time: 2019-03-17 16:06:53
  */
 
 <template>
     <el-row>
 
         <el-dialog width="300px" title="登录" :visible.sync="dialogForLogin" :lock-scroll=false>
-            <el-form :model="form">
-                <el-form-item label="登录账号">
-                    <el-input v-model="form.name" placeholder="请输入登录账号"></el-input>
+            <el-form :rules="rules" :model="form" :ref="form">
+                <el-form-item label="登录邮箱" prop="name">
+                    <el-input v-model="form.name" placeholder="请输入登录邮箱"></el-input>
                 </el-form-item>
-                <el-form-item label="登录密码">
+                <el-form-item label="登录密码" prop="password">
                     <el-input v-model="form.password" type="password" placeholder="请输入登录密码"></el-input>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button class="login_button" type="primary" @click="dialogFormVisible = false">确 定</el-button>
+                <el-button class="login_button" type="primary" @click="submitForm(form)">确 定</el-button>
             </div>
         </el-dialog>
 
@@ -28,17 +28,78 @@
 .login_button {
   width: 100%;
 }
+.el-dialog__wrapper {
+  z-index: 555555 !important;
+}
 </style>
 <script>
+import { apiMallLogin } from "../../../assets/js/axios/api.js";
 export default {
   data() {
+    var checkEmail = (rule, value, callback) => {
+      const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+      if (!value) {
+        return callback(new Error("登录邮箱不得为空"));
+      }
+      setTimeout(() => {
+        if (mailReg.test(value)) {
+          callback();
+        } else {
+          callback(new Error("请输入正确的邮箱格式"));
+        }
+      }, 100);
+    };
     return {
       dialogForLogin: false,
       form: {
         name: "",
         password: ""
+      },
+
+      rules: {
+        name: [
+          { validator: checkEmail, required: true, trigger: "blur" } //邮箱的验证规则
+        ],
+        password: [
+          { required: true, message: "登录密码不得为空", trigger: "blur" } //密码的验证规则
+        ]
       }
     };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          apiMallLogin({
+            email: this.form.name,
+            password: this.form.password
+          }).then(res => {
+            console.log();
+            if (res.data.msg == "请求成功") {
+              this.axios({
+                method: "POST",
+                url: "api/login/adminLogin/getAdminTo",
+                headers: {
+                  token: res.data.data.token
+                }
+              }).then(rese => {
+                this.$emit("storeName", rese.data.data.name);
+                this.Cookie.setCookie("storeName", rese.data.data.name);
+                this.Cookie.setCookie("storeToken", res.data.data.token);
+              });
+            } else if (res.data.msg == "邮箱或密码错误") {
+              this.$message({
+                message: "邮箱或密码错误",
+                type: "warning"
+              });
+            }
+          });
+        } else {
+          console.log("error submit!!");
+          // return false;
+        }
+      });
+    }
   }
 };
 </script>   
