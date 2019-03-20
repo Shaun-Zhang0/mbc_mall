f5029e17a911c49488d1811928293a24a6b54925
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:35 +0800

    Update orderlist.vue

--- src/components/mall/order/orderlist.vue
+++ src/components/mall/order/orderlist.vue
@@ -1,3 +1,10 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-03-20 14:57:34 
+ * @Last Modified by:   Shaun.Zhang 
+ * @Last Modified time: 2019-03-20 14:57:34 
+ */
+
 <template>
     <div>
         <index-header></index-header>
