8cd02c92ffbf0ad2dee8faec20ad5a0ec6df3a05
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:41 +0800

    Update search.vue

--- src/components/mall/search/search.vue
+++ src/components/mall/search/search.vue
@@ -1,3 +1,10 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-03-20 14:57:48 
+ * @Last Modified by:   Shaun.Zhang 
+ * @Last Modified time: 2019-03-20 14:57:48 
+ */
+
 <template>
     <div>
         <div>
