31806481ce1c4d754ac9d50ff330837648a25e87
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:44:40 +0800

    Update cookie.js

--- src/assets/js/cookie.js
+++ src/assets/js/cookie.js
@@ -1,9 +1,16 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-03-20 14:53:15 
+ * @Last Modified by: Shaun.Zhang
+ * @Last Modified time: 2019-03-20 20:20:10
+ */
+
 const setCookie = function setCookie(name,value) 
 { 
     // var Days = 30; 
     var exp = new Date(); 
     exp.setTime(exp.getTime() + 120*60*1000); 
-    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString() + ";path=/";
 } 
 
 //读取cookies 
