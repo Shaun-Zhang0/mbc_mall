08f196c0c50caa210553ec90641c8f071e857626
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:44:42 +0800

    Update timeFormat.js

--- src/assets/js/timeFormat.js
+++ src/assets/js/timeFormat.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-03-20 14:53:21 
+ * @Last Modified by:   Shaun.Zhang 
+ * @Last Modified time: 2019-03-20 14:53:21 
+ */
+
 export const formatTime = time =>{
     var times = new Date(+new Date(time) + 8 * 3600 * 1000)
     .toISOString()
