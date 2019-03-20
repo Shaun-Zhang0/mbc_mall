c54068ed7bdbd4b62bd62d1ec1327233657a8df3
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:47 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,32 +1,24 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 /**厂商管理平台 */
-import Orderlist from "./components/brand/order/orderlist.vue";
-import Index from "./components/brand/structure/home.vue";
-import Release_product from "./components/brand/product/release_product.vue";
-import Productlist from "./components/brand/product/productlist.vue";
-import Update_info from "./components/brand/information/update_info.vue";
-import Chargerback from "./components/brand/order/chargeback.vue";
-import Login from "./components/brand/login/login.vue";
-import register from "./components/brand/register/register.vue";
+import Login from "./components/brand/login/login.vue"; //登录
+import register from "./components/brand/register/register.vue"; //注册
+import Orderlist from "./components/brand/order/orderlist.vue"; //订单列表
+import Index from "./components/brand/structure/home.vue"; //主页展示
+import Release_product from "./components/brand/product/release_product.vue"; //发布商品
+import Productlist from "./components/brand/product/productlist.vue"; //商品列表
+import Update_info from "./components/brand/information/update_info.vue"; //更新个人信息
+import finance from "./components/brand/finance/finance.vue"; //账单管理
+
 /**进货平台 */
-import mall_index from "./components/mall/index/index.vue";
-import mall_login from "./components/mall/login/login.vue";
-import index_header from "./components/mall/index/header.vue";
-import carousel from "./components/mall/index/carousel.vue";
-import search from "./components/mall/index/search.vue";
-import footer from "./components/mall/index/footer.vue";
-import product_line from "./components/mall/index/product_line.vue";
-/**商品页面 */
-import product from "./components/mall/product/product.vue";
-/**确认订单 */
+import mall_index from "./components/mall/index/index.vue"; //主页
+import mall_login from "./components/mall/login/login.vue"; //登录
+import product from "./components/mall/product/product.vue"; //商品详情页
 import order from "./components/mall/buy/order.vue";
-/**订单列表 */
 import orderlist from "./components/mall/order/orderlist.vue";
 import orderinfo from "./components/mall/order/orderinfo.vue";
-
 import searchResult from "./components/mall/search/search.vue";
+
 Vue.use(Router);
 
 export default new Router({
@@ -35,94 +27,81 @@ export default new Router({
   routes: [
     /**厂商管理平台 */
     {
-      path: "/brand",
+      path: "/brand", //管理平台的首页
       component: Index,
       meta: { keepAlive: true }
     },
     {
-      path: "/brand/update_info",
-      name: 'Update_info',
+      path: "/brand/update_info", //更新信息页面
+      name: "Update_info",
       component: Update_info,
       meta: { keepAlive: true }
     },
     {
-      path: "/brand/orderlist",
-      name: 'Orderlist',
+      path: "/brand/orderlist", //订单列表页面
+      name: "Orderlist",
       component: Orderlist,
       meta: { keepAlive: true }
     },
     {
-      path: "/brand/release_product",
-      name: 'Release_product',
+      path: "/brand/release_product", //发布商品页面
+      name: "Release_product",
       component: Release_product,
       meta: { keepAlive: true }
     },
     {
-      path: "/brand/Productlist",
-      name: 'Productlist',
+      path: "/brand/Productlist", //商品列表页面
+      name: "Productlist",
       component: Productlist,
       meta: { keepAlive: true }
     },
+
     {
-      path: "/brand/Chargerback",
-      name: 'Chargerback',
-      component: Chargerback,
-      meta: { keepAlive: true }
-    },
-    {
-      path: "/brand/Login",
-      name: 'Login',
+      path: "/brand/Login", //登录页面
+      name: "Login",
       component: Login,
       meta: { keepAlive: false }
     },
     {
-      path: "/brand/register",
-      name: 'register',
+      path: "/brand/register", //注册页面
+      name: "register",
       component: register,
       meta: { keepAlive: false }
     },
     /**进货平台 */
-    {//header
-      path: "/mall/index",
-      name: 'mall_index',
+    {
+      path: "/mall/index", //首页
+      name: "mall_index",
       component: mall_index,
       meta: { keepAlive: false }
     },
-    {//header
+    {
       path: "/mall/myorderlist",
-      name: 'orderlist',
+      name: "orderlist",
       component: orderlist,
       meta: { keepAlive: false }
     },
-    // {//header
-    //   path: "/mall/order",
-    //   name: 'order',
-    //   component: order,
-    //   meta: { keepAlive: false }
-    // },
-    // {//订单详情页
-    //   path: "/mall/myorderlist/orderinfo",
-    //   name: 'orderinfo',
-    //   component: orderinfo,
-    //   meta: { keepAlive: false }
-    // },
-    {//商品详情页
-      path: "/mall/product/:id",
-      name: 'product',
+
+    {
+      path: "/brand/finance", //财务管理界面
+      name: "finance",
+      component: finance,
+      meta: { keepAlive: true }
+    },
+    {
+      path: "/mall/product/:id", //商品详情页面
+      name: "product",
       component: product,
       meta: { keepAlive: false }
     },
     {
-      path: "/mall/searchResult",
-      name: 'searchResult',
+      path: "/mall/searchResult", //搜索页面
+      name: "searchResult",
       component: searchResult,
       meta: { keepAlive: false },
-      query:{
-        product_name:""
+      query: {
+        product_name: ""
       }
-      
     }
   ]
-  
 });
-    
