569b00b95669d81f96a41f2f80fece89f0d9b95c
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-20 23:45:03 +0800

    Update footer.vue

--- src/components/brand/structure/footer.vue
+++ src/components/brand/structure/footer.vue
@@ -1,3 +1,10 @@
+/*
+ * @Author: Shaun.Zhang 
+ * @Date: 2019-03-20 14:55:41 
+ * @Last Modified by:   Shaun.Zhang 
+ * @Last Modified time: 2019-03-20 14:55:41 
+ */
+
 <template>
     <footer>
         <div style="text-align:center">version 0.1.0</div>
