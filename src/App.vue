6a58e19ede711491ce2c089aab70312d657d8c27
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-23 00:50:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,8 +39,16 @@ export default {
     chargeback: Chargeback, //退单的组件
     login: login, //登录的组件
     register: register //注册的组件
-  }
-};
+  },
+  mounted(){
+	window.addEventListener('hashchange',()=>{
+		var currentPath = window.location.hash.slice(1); // 获取输入的路由
+		if(this.$router.path !== currentPath){
+			this.$router.push(currentPath); // 动态跳转
+		}
+	},false);
+}};
+
 </script>
 
 <style>
