de4d28e41d190ae333a1420a25d320fa3498af47
Shaun-Zhang0 <47142236+Shaun-Zhang0@users.noreply.github.com>
2019-03-23 23:32:29 +0800

    Update login.vue

--- src/components/mall/login/login.vue
+++ src/components/mall/login/login.vue
@@ -2,27 +2,27 @@
  * @Author: Shaun.Zhang 
  * @Date: 2019-02-25 16:15:05 
  * @Last Modified by: Shaun.Zhang
- * @Last Modified time: 2019-03-17 16:06:53
+ * @Last Modified time: 2019-03-23 21:32:18
  */
 
 <template>
-    <el-row>
+  <el-row>
 
-        <el-dialog width="300px" title="登录" :visible.sync="dialogForLogin" :lock-scroll=false>
-            <el-form :rules="rules" :model="form" :ref="form">
-                <el-form-item label="登录邮箱" prop="name">
-                    <el-input v-model="form.name" placeholder="请输入登录邮箱"></el-input>
-                </el-form-item>
-                <el-form-item label="登录密码" prop="password">
-                    <el-input v-model="form.password" type="password" placeholder="请输入登录密码"></el-input>
-                </el-form-item>
-            </el-form>
-            <div slot="footer" class="dialog-footer">
-                <el-button class="login_button" type="primary" @click="submitForm(form)">确 定</el-button>
-            </div>
-        </el-dialog>
+    <el-dialog width="300px" title="登录" :visible.sync="dialogForLogin" :lock-scroll=false>
+      <el-form :rules="rules" :model="form" :ref="form">
+        <el-form-item label="登录邮箱" prop="name">
+          <el-input v-model="form.name" placeholder="请输入登录邮箱"></el-input>
+        </el-form-item>
+        <el-form-item label="登录密码" prop="password">
+          <el-input v-model="form.password" type="password" placeholder="请输入登录密码"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button class="login_button" type="primary" @click="submitForm(form)">确 定</el-button>
+      </div>
+    </el-dialog>
 
-    </el-row>
+  </el-row>
 </template>
 <style scoped>
 .login_button {
@@ -34,6 +34,8 @@
 </style>
 <script>
 import { apiMallLogin } from "../../../assets/js/axios/api.js";
+import { apiGetStoreNameapi } from "../../../assets/js/axios/api.js";
+import Cookie from "../../../assets/js/cookie.js";
 export default {
   data() {
     var checkEmail = (rule, value, callback) => {
@@ -74,18 +76,18 @@ export default {
             email: this.form.name,
             password: this.form.password
           }).then(res => {
-            console.log();
+            console.log(res);
             if (res.data.msg == "请求成功") {
-              this.axios({
-                method: "POST",
-                url: "api/login/adminLogin/getAdminTo",
-                headers: {
-                  token: res.data.data.token
+              this.Cookie.setCookie("storeToken", res.data.data.token);
+              apiGetStoreNameapi(
+                {},
+                {
+                  headers: { token: this.Cookie.getCookie("storeToken") }
                 }
-              }).then(rese => {
+              ).then(rese => {
+                console.log(rese);
                 this.$emit("storeName", rese.data.data.name);
                 this.Cookie.setCookie("storeName", rese.data.data.name);
-                this.Cookie.setCookie("storeToken", res.data.data.token);
               });
             } else if (res.data.msg == "邮箱或密码错误") {
               this.$message({
